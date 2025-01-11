5156ea25460d2599953a98f81f46e4fc4e224a5e
Jonas <jonas.lillebo.haugen@politiet.no>
2025-01-11 21:16:18 +0100

    Border radius animation and hr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { GoogleSearchBar } from "@/components"
+import "./HomePage.scss"
 
 export default function Home() {
   return (
-    <div className="Home">
+    <div className="HomePage">
       <GoogleSearchBar />
+      <hr />
     </div>
   )
 }
