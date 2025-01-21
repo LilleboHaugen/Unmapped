10a4141c14e569d17562f60729225fcb8e35c99d
Jonas <jonas.lillebo.haugen@politiet.no>
2025-01-21 21:05:26 +0100

    tseslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+import { FlatCompat } from "@eslint/eslintrc"
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintConfigPrettier from "eslint-config-prettier"
 import { dirname } from "path"
 import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -9,17 +12,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "prettier", // Add Prettier here
-  ),
+export default [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslint.configs.recommended,
+  eslintConfigPrettier,
   {
     rules: {
-      "@typescript-eslint/no-empty-object-type": "off", // Add any custom rules here
+      ...tseslint.configs.strictTypeChecked.rules,
+      ...tseslint.configs.stylisticTypeChecked.rules,
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 ]
-
-export default eslintConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import "./HomePage.scss"
 
 export default function HomePage() {
-  return <div className="HomePage"></div>
+  return <div className="HomePage">HomePage</div>
 }
